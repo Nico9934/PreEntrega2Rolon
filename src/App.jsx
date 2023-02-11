dde672f718f0ce8f1bb64535a3f7c23822f962ba
Nico9934 <nico9934@gmail.com>
2023-02-11 14:14:49 -0300

    modificacion al app

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import CarritoProvider from "./components/CarritoProvider";
 import Footer from "./components/Footer";
-import ItemDetailContainer from "./components/ItemDetailContainer";
-import ItemListContainer from "./components/ItemListContainer";
 import MainRoutes from "./components/MainRoutes";
 import NavBar from "./components/NavBar";
 
